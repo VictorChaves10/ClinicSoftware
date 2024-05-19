929d9c4d839f3ebb76a1da2da9ffe9ac777c7ff6
Victor Chaves <victor.chaves460@gmail.com>
2024-05-19 14:21:54 -0300

    feat: added JQueryDataBase CDN

--- ViewModels/ClientListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ClinicSoftware.Models;
-
-namespace ClinicSoftware.ViewModels
-{
-    public class ClientListViewModel
-    {
-        public IEnumerable<Client> ? Clients { get; set; }
-    }
-}
